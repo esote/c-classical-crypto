f77f9956aa05aeeff208cf1355d68e949e88705d
Esote <esote@users.noreply.github.com>
2018-05-18 11:23:33 -0500

    Update caesar-cipher.c

--- caesar-cipher/caesar-cipher.c
+++ caesar-cipher/caesar-cipher.c
@@ -1,18 +1,12 @@
 /* caesar-cipher -- rotate strings through the alphabet */
 
+#include <error.h>
+#include <errno.h>
 #include <getopt.h>
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define PROGRAM_NAME "caesar-cipher"
-
-/* Rotate once by default */
-#define DEFAULT_ROTATIONS 1
-
-/* Convert string to uintmax_t in base 10 */
-#define STRTOUMAX_BASE 10
 
 /* Standard 26-character alphabet */
 #define ALPHABET_SIZE 26
@@ -31,20 +25,18 @@ static struct option const long_opts[] = {
 	{"no-shortcut", no_argument, NULL, 's'},
 	{"numbers", no_argument, NULL, 'n'},
 	{"rotations", required_argument, NULL, 'r'},
+
 	{NULL, 0, NULL, 0}
 };
 
-_Noreturn void usage(int const status)
+static _Noreturn void usage(int const status, char const *const name)
 {
+	printf("Usage: %s [OPTION]... [STRING]...\n", name);
 	if(status != EXIT_SUCCESS) {
-		fprintf(stderr, "Usage: %s [OPTION]... [STRING]...\n",
-				PROGRAM_NAME);
-		fprintf(stderr, "Try '%s --help' for more information.\n",
-				PROGRAM_NAME);
+		fprintf(stderr, "Try '%s --help' for more information.\n", name);
 	} else {
-		printf("Usage: %s [OPTION]... [STRING]...\n", PROGRAM_NAME);
 		puts("Rotate strings through the alphabet.");
-		printf("Example: %s -n -r 25 \"Hello 123 World!\"\n", PROGRAM_NAME);
+		printf("Example: %s -n -r 25 \"Hello 123 World!\"\n", name);
 		puts("\nOptions:\n\
   -h, --help             display this help text and exit\n\
   -s, --no-shortcut      do not use a shortcut to reduce\n\
@@ -58,7 +50,7 @@ _Noreturn void usage(int const status)
 }
 
 /* Assumes contiguous character encoding from a-z A-Z */
-char rotate_char(char const ch, uintmax_t const rotations)
+static char rotate_char(char const ch, uintmax_t const rotations)
 {
 	uintmax_t rot_ch;
 
@@ -76,7 +68,7 @@ char rotate_char(char const ch, uintmax_t const rotations)
 	return (char)(rot_ch + (((uintmax_t)ch - rot_ch + rotations) % mod));
 }
 
-void caesar_cipher(char const *const string, uintmax_t const rotations)
+static void caesar_cipher(char const *const string, uintmax_t const rotations)
 {
 	for(size_t i = 0; string[i]; ++i)
 		putchar(rotate_char(string[i], rotations));
@@ -87,7 +79,8 @@ int main(int const argc, char *const *const argv)
 	rotate_numbers = false;
 	bool rotation_shortcut = true;
 
-	uintmax_t rotations = DEFAULT_ROTATIONS;
+	/* Rotate once by default */
+	uintmax_t rotations = 1;
 
 	static char const *const default_strings_list[] = {NULL};
 	char const *const *strings_list;
@@ -97,21 +90,24 @@ int main(int const argc, char *const *const argv)
 	while((c = getopt_long(argc, argv, "hnr:s", long_opts, NULL)) != -1) {
 		switch(c) {
 			case 'h':
-				usage(EXIT_SUCCESS);
+				usage(EXIT_SUCCESS, argv[0]);
 				break;
 			case 'n':
 				rotate_numbers = true;
 				break;
 			case 'r':
-				rotations = strtoumax(optarg, NULL, STRTOUMAX_BASE);
+				rotations = strtoumax(optarg, NULL, 10);
 				break;
 			case 's':
 				rotation_shortcut = false;
 				break;
 			default:
-				usage(EXIT_FAILURE);
+				usage(EXIT_FAILURE, argv[0]);
 		}
 	}
+
+	if(rotations == UINTMAX_MAX && errno == ERANGE)
+		error(EXIT_FAILURE, 0, "could not convert to integer (overflow)");
 
 	if(rotation_shortcut) {
 		uintmax_t const mod = rotate_numbers ? LCM_ALPHA_NUM : ALPHABET_SIZE;
